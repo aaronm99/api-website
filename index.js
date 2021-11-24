20974d8d00e6e6b03fd62ca14b63e83311f62c4a
Aaron Mistry <aaron.rohin@hotmail.co.uk>
2021-11-24 23:48:15 +0000

    enter key now submits search

--- index.js
+++ index.js
@@ -25,3 +25,13 @@ function getData() {
       });
     });
 }
+
+function searchKeyPress(e) {
+  // look for window.event in case event isn't passed in
+  e = e || window.event;
+  if (e.keyCode == 13) {
+    document.getElementById("search--btn").click();
+    return false;
+  }
+  return true;
+}
